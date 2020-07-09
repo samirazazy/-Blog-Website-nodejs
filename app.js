66256107742dcd6d6e3fc9521e86d3171d3ce324
samir azazy <samiralazazy@gmail.com>
2020-07-09 12:12:10 +0200

    add posts from compose page

--- app.js
+++ app.js
@@ -18,8 +18,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent });
+  res.render("home", { 
+    homeStartingContent, 
+    posts 
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -32,11 +37,15 @@ app.get("/contact", function (req, res) {
 
 app.get("/compose", function (req, res) {
   res.render("compose", { contactContent });
-  console.log(res.body);
 });
 
 app.post("/compose", function (req, res) {
-  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
